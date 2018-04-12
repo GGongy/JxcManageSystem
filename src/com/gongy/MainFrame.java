bf0e5bcd520f00aa95113735062a435f5e632f4b
gongyu <735701336@qq.com>
2018-04-12 16:36:13 +0800

    添加登录窗体 添加 main 方法

---
+++ src/com/gongy/MainFrame.java
@@ -0,0 +1,4 @@
+package com.gongy;
+
+public class MainFrame {
+}
--- src/com/gongy/login/LoginDialog.java
+++ src/com/gongy/login/LoginDialog.java
@@ -1,4 +1,20 @@
 package com.gongy.login;
 
-public class LoginDialog {
+import com.gongy.MainFrame;
+
+import javax.swing.*;
+
+// 登录窗体
+public class LoginDialog extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private LoginPanel loginPanel = null;       // 登录面板
+    private JLabel jLabel = null;               // “用户名”标签
+    private JTextField userField = null;        // “用户名”文本框
+    private JLabel jLabel1 = null;              // “密码”标签
+    private JPasswordField passwordField = null;// “密码”文本框
+    private JButton loginButton = null;         // “登录”按钮
+    private JButton exitButton = null;          // “退出”按钮
+    private static String userStr;              // "用户名“文本框中的内容
+    private MainFrame mainFrame;                // 主窗体
+
 }
